966d630e75a51b68b10d8cc89965cf4a2e382839
Samed <samed.skulj08@gmail.com>
2021-02-07 16:40:11 +0100

    Pocetak i plan izgradnje

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,18 @@
 
+import React, {useState} from "react"
 import './App.css';
-
+import {Button, Container, Row, Col} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [alarm, setAlert] = useState();
+
   return (
-    <div className="App">
-        <p>Hello World</p>
-    </div>
+    <>
+   <Container fluid className = "d-flex justify-content-center align-items-center w-25 pt-3">
+     <Button>Zdravo!</Button>
+   </Container>
+    </>
+   
   );
 }
 
