e1b264e6602ea57f333d83ad28562f12d9d6629e
Samed <samed.skulj08@gmail.com>
2021-02-07 20:18:17 +0100

    Fetch API implementiran

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,24 +1,49 @@
 
 import React, {useState} from "react"
 import './App.css';
-import {Button, Container, InputGroup, FormControl, Row, Col} from "react-bootstrap"
+import {Button, Container, Form, FormControl, Row, Col} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
-  const [alarm, setAlert] = useState();
+
+
+  const[imeGrada, setImeGrada] = useState("");
+  const[vrijeme, setVrijeme] = useState({});
+
+
+  function fetchVremena() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${imeGrada}&APPID=07b663c40260196298d7045ac1f80f5c`)
+    .then(response => response.json())
+    .then(data => {
+      setVrijeme(data)
+      console.log(data);
+    });
+
+  }
+  
 
   return (
     <>
    <Container>
      <Row className = "justify-content-between align-items-center moj-red">
-       <Col className = "col-3 moja-kolona align-self-start">made by Samed</Col>
+       <Col className = "col-3 moja-kolona align-self-center">
+         <div>
+           <p>{vrijeme.main.humidity}</p>
+           <p id = "temperatura">{vrijeme.main.temp}</p>
+         </div>
+       </Col>
        <Col className = "col-6 moja-kolona">
-         <InputGroup size="sm" className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-  </InputGroup>
-  <Button>Pritisnite</Button>
+       <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label id = "lokacija">Lokacija</Form.Label>
+    <Form.Control type="text" placeholder="Npr. London..." onChange = {e => setImeGrada(e.target.value)} />
+    <Form.Text className="text-muted">
+      Molimo Vas upišite ime lokacije!
+    </Form.Text>
+  </Form.Group>
+  </Form>
+  <Button onClick = {fetchVremena}>Pritisnite</Button>
   </Col>
      </Row>
    </Container>
