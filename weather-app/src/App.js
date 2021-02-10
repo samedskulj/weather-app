e8d5f03aa54df88acd3e204e80615a7b1d6ae233
Samed <samed.skulj08@gmail.com>
2021-02-10 19:36:34 +0100

    Major error issues fixed

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,67 +1,101 @@
-
-import React, {useState} from "react"
-import './App.css';
-import {Button, Container, Form, FormControl, Row, Col} from "react-bootstrap"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import React, { useState } from "react";
+import "./App.css";
+import ErrorUnos from "./components/ErrorUnos";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Row,
+  Col,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 import cloudy from "./slike/cloudy.png";
-import {MDBInput, MDBBtn, MDBIcon} from "mdbreact";
+import { MDBInput, MDBBtn, MDBIcon } from "mdbreact";
 import Poruka from "./components/Poruka";
 
 function App() {
-
   let mojBody = document.querySelector("#temperatura2");
-  const[imeGrada, setImeGrada] = useState("");
-  const[vrijeme, setVrijeme] = useState({});
-
+  const [imeGrada, setImeGrada] = useState("");
+  const [vrijeme, setVrijeme] = useState({});
+  const [error, setError] = useState("");
 
   function fetchVremena() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${imeGrada}&APPID=07b663c40260196298d7045ac1f80f5c`)
-    .then(response => {
-      if(response.ok) {
-        return response.json()
-      } else {
-        return Promise.reject({
-          status: response.status,
-          statusText: response.statusText,
+    if (imeGrada === "" || imeGrada === undefined) {
+      setError("Molimo Vas upisite grad!!");
+    } else {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${imeGrada}&APPID=07b663c40260196298d7045ac1f80f5c`
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return Promise.reject({
+              status: response.status,
+              statusText: response.statusText,
+            });
+          }
         })
-      }
-    })
-    .then(data => {
-      setVrijeme(data)
-      setImeGrada("");
-      console.log(data);
-    }).catch(error => {
-      if(error.status === 400) {
-        alert(error.statusText +": " +  "Niste ništa ukucali ili ste upisali pogrešan grad, pokušajte ponovo")
-      }
-    })
-  };
+        .then((data) => {
+          setVrijeme(data);
+          setImeGrada("");
+          setError("");
+          console.log(data);
+        })
+        .catch((error) => {
+          if (error.status === 404) {
+            setError("Grad koji ste upisali ne postoji!!");
+          }
+        });
+    }
+  }
   return (
-    <> 
-    
-    <div className= "test">
-    <Poruka></Poruka>
-    </div>
-   <Container className = "">
-     <Row className = "justify-content-between align-items-center moj-red">
-       <Col className = "col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 moja-kolona align-self-center">
-       {(typeof vrijeme.main != "undefined") ? (
-         <div id = "informacije">
-           <p id = "temperatura2">{Math.floor((vrijeme.main.temp - 273.19))}°c</p>
-           <p id = "imegrada1" className = "align-self-center ml-2 mt-2">{vrijeme.name}</p>
-           <img src = {cloudy} className = "align-self-center mr-1 ml-2 mb-2" height = "30px"></img>
-         </div>
-          ): ("")}
-       </Col>
-       <Col className = "col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 moja-kolona1">
-        <MDBInput label = "Npr London..." value = {imeGrada} onChange = {e => setImeGrada(e.target.value)} />
-        <MDBBtn id = "dugme1" icon = "sun "rounded color="blue" className = "mb-5" onClick = {fetchVremena}>Izaberite</MDBBtn>
-  </Col>
-     </Row>
-   </Container>
+    <>
+      <Container className="">
+        <Row className="justify-content-between align-items-center moj-red">
+          <Col className="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 moja-kolona align-self-center">
+            {typeof vrijeme.main != "undefined" ? (
+              <div id="informacije">
+                <p id="temperatura2">
+                  {Math.floor(vrijeme.main.temp - 273.19)}°c
+                </p>
+                <p id="imegrada1" className="align-self-center ml-2 mt-2">
+                  {vrijeme.name}
+                </p>
+                <img
+                  src={cloudy}
+                  className="align-self-center mr-1 ml-2 mb-2"
+                  height="30px"
+                ></img>
+              </div>
+            ) : (
+              ""
+            )}
+          </Col>
+          <Col className="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-12 moja-kolona1">
+            <h6 className="errorText">{error}</h6>
+            <MDBInput
+              label="Npr London..."
+              value={imeGrada}
+              onChange={(e) => setImeGrada(e.target.value)}
+            />
+
+            <MDBBtn
+              id="dugme1"
+              icon="sun "
+              rounded
+              color="blue"
+              className="mb-5"
+              onClick={() => fetchVremena()}
+            >
+              Izaberite
+            </MDBBtn>
+          </Col>
+        </Row>
+      </Container>
     </>
-   
   );
 }
 
